3d4b742ca39fa0fec4b28d5307b1a5090bfdd87b
Wangqiwang <wqw21@bupt.edu.cn>
2021-08-02 00:51:08 +0800

    dataset

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from data import DogCat
-
+from torchvision import datasets, models, transforms
+from torch.utils.data import DataLoader
 
 if __name__ == '__main__':
     print('hello')
-    dataset = DogCat('train', True, 0)
+    dataset = DogCat('train', True, 1)
+    trainloader = DataLoader(dataset,
+                             batch_size=256,
+                             shuffle=True,
+                             num_workers=12)
+    
+    for ii, (data, label) in enumerate(trainloader):
+        print(label)
+
+    print(dataset.imgs)
